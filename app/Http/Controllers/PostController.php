5617b0483a89e9efe675978834d2572f8ef05b3a
a120017 <a120017@g.kogakuin.jp>
2023-03-23 20:02:12 +0900

    create error message

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -22,14 +23,14 @@ class PostController extends Controller
     public function create()
     {
         return view('posts/create');
-        
     }
     
     
-    public function store(Request $request, Post $post)
+    public function store(PostRequest $request, Post $post)
     {
+        
         $input = $request['post'];
         $post->fill($input)->save();
-        return redirect('/posts/' , $post->id);
+        return redirect('/posts/' . $post->id);
     }
 }
